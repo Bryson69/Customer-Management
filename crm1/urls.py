383c661b41847764ae6e4925626c4db80a3581d7
Bryson69 <brysonmundia@gmail.com>
2020-08-26 16:22:04 +0300

    Initial commit

---
+++ crm1/urls.py
@@ -0,0 +1,26 @@
+"""crm1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+
+# When a user types in a url path its the job to find the
+# pattern that the user has typed into the browser and
+# return them back to a page that the user is looking for
+urlpatterns = [
+    path(r'^admin/', admin.site.urls),
+    path('',include('accounts.urls'))
+]
