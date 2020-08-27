a59d409d4eada85a07f08e0c73ff9ff955472855
Bryson69 <brysonmundia@gmail.com>
2020-08-27 14:29:01 +0300

    Add comment

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+#
+#Url paths are found in order to trigger the function on views.py
 urlpatterns = [
     path('', views.home),
     path('products/', views.products),
