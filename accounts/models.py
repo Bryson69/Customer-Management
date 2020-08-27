f6cdfa8742fcf4646a1185745ec0d36b454bc37a
Bryson69 <brysonmundia@gmail.com>
2020-08-27 16:11:21 +0300

    Add function

--- accounts/models.py
+++ accounts/models.py
@@ -18,6 +18,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    # Return name for it to display it in the admin panel
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORY = (
         ('Indooor', 'Indooor'),
@@ -27,8 +34,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
