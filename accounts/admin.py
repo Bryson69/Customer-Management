520f4ff5368bfee2ff5adb6daf9d33ead55a9245
Bryson69 <brysonmundia@gmail.com>
2020-08-27 14:28:37 +0300

    Register the Customer, Product and Order table

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+# Import the class Customer
 
 # Register your models here.
+# Registering the table from the class Customer
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
