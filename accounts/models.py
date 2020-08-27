bfeb9d264abf2bef204b1664c15ad0f486dd253f
Bryson69 <brysonmundia@gmail.com>
2020-08-27 14:07:10 +0300

    Add class Customer and fuction to return a name to dispslay a name on the admin panel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+# Allow to create classes that create database tables.
+#
 # Create your models here.
+
+# String values are specified as --> Charfield .
+# DateTimeField -->Lets THe user know when a specific Item was loaded.
+# Null = True -->Allows us to make changes into our database, 
+# Allows to import customer with maybe just a name and no phone or email or date created. 
+# If null is not set to true There will be an error and we will be forced to add a defult value.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    # Return name for it to display it in the admin panel
+    def __str__(self):
+        return self.name
