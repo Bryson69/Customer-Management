8479e94d487e6b39aa55b3277b1bda051d8ff2e8
Bryson69 <brysonmundia@gmail.com>
2020-08-27 14:24:02 +0300

    Add class Product, Order and Input a status and choices tuple which will have values

--- accounts/models.py
+++ accounts/models.py
@@ -12,8 +12,31 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     # Return name for it to display it in the admin panel
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indooor', 'Indooor'),
+        ('Out door', 'Out door'),
+        )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out of delivery', 'Out of delivery'),
+        ('Delivered', 'Delivered'),
+        )
+    #customer = 
+    #product = 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
