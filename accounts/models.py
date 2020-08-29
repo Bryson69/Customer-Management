a32a20bbbf60bb5847e3bb2d99b4dca17ad764cf
Bryson69 <brysonmundia@gmail.com>
2020-08-29 14:11:11 +0300

    Add a field for note

--- accounts/models.py
+++ accounts/models.py
@@ -51,6 +51,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)# on_delete=models.SET_NULL --> When an order is removed the order will remain in the database with a no value for the customer
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.product.name
