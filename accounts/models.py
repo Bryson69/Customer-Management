e440a50a6495435c7942d94a4afd76a75cc132f9
Bryson69 <brysonmundia@gmail.com>
2020-09-01 17:19:07 +0300

    Add model profile_pic

--- accounts/models.py
+++ accounts/models.py
@@ -14,6 +14,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(default = "profile1.png",null = True, blank = True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     # Return name for it to display it in the admin panel
