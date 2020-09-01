1f8f0b62207ebb8fa1a556f9f8d685bffbcb0c19
Bryson69 <brysonmundia@gmail.com>
2020-09-01 17:16:23 +0300

    Import CustomerForm, Add accountSettings view function in order to render information on the user adn the profile_pic of the user

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory#ways of creating multiple forms within one form
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -93,6 +93,23 @@ def userPage(request):
     context = {'orders':orders, 'total_orders':total_orders, 'delivered':delivered,
     'pending':pending}
     return render(request, 'accounts/user.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+	customer = request.user.customer
+	form = CustomerForm(instance=customer)
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST, request.FILES,instance=customer)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'accounts/account_settings.html', context)
+
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
