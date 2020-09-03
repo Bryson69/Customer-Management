7dfacad7ac22faa38a737df29ecc3ef527300fd8
Bryson69 <brysonmundia@gmail.com>
2020-09-03 23:49:34 +0300

    Add SMTP

--- crm1/settings.py
+++ crm1/settings.py
@@ -131,10 +131,3 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 #Simple Mail Transfer Protocol (SMTP) Configuration
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '*********'
-EMAIL_HOST_PASSWORD = '*********'
